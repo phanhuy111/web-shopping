442fcb5041ce0508c184b9862c3360f4676bda0c
phanhuy111 <phanhuypy.1999@gmail.com>
2022-08-31 17:32:18 +0700

    aaaa

--- src/Component/Product.js
+++ src/Component/Product.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
-import Header from './HomePage/Header';
-import Footer from './HomePage/Footer';
-import Line from './Line/Line';
-import InforProduct from './Product/InforProduct';
-import OneProduct from './Product/OneProduct';
-import ModalProduct from './Product/ModalProduct';
-import Bonus from './Product/Bonus';
-
+import React, { Component } from "react";
+import Header from "./HomePage/Header";
+import Footer from "./HomePage/Footer";
+import Line from "./Line/Line";
+import InforProduct from "./Product/InforProduct";
+import OneProduct from "./Product/OneProduct";
+import ModalProduct from "./Product/ModalProduct";
+import Bonus from "./Product/Bonus";
 
 class Product extends Component {
-    render() {
-        return (
-            <div>
-                <Header />
-                <Line />
-                <div>
-                    <OneProduct />
-                    <InforProduct />
-                    <ModalProduct />
-                    <Bonus />
-                </div>
-                <div className="footer_pro" style={{ marginTop:'160%' }}>
-                   <Footer />
-                </div>   
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Line />
+        <p>{"hello"}</p>
+        <div>
+          <OneProduct />
+          <InforProduct />
+          <ModalProduct />
+          <Bonus />
+        </div>
+        <div className="footer_pro" style={{ marginTop: "160%" }}>
+          <Footer />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Product;
